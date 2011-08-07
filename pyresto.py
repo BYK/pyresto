9e81468c95cd73fa1df4b7311474e2cfec019037
Burak Yigit Kaya <madbyk@gmail.com>
2011-08-07 22:10:24 +0300

    Fixed incomplete indict -> in_dict change. Turned examples directory into a package for easy testing. Fixed some styling. Added Foreign for foreign keys. Added User.Followers, Repo.contributors, Repo.tags and Repo.branches to example GitHub implementation.

--- pyresto.py
+++ pyresto.py
@@ -3,89 +3,47 @@ import json
 import logging
 from urllib import quote
 
-__all__ =['Model', 'Many']
+__all__ = ['Model', 'Many', 'Foreign']
 
 logging.getLogger().setLevel(logging.DEBUG)
 
 class ModelBase(type):
   def __new__(cls, name, bases, attrs):
-    if name == 'Model' or name == 'View':
+    if name == 'Model':
       return super(ModelBase, cls).__new__(cls, name, bases, attrs)
     new_class = type.__new__(cls, name, bases, attrs)
-    
+
     if not hasattr(new_class, '_path'):
       new_class._path = '/%s/%%(id)s' % quote(name.lower())
 
     conn_class = httplib.HTTPSConnection if new_class._secure else httplib.HTTPConnection
     new_class._connection = conn_class(new_class._host)
-    
+
     return new_class
-
-
-class Model(object):
-  __metaclass__ = ModelBase
-  _secure = True
-  _continuator = lambda x,y:None
-  _parser = staticmethod(json.loads)
-
-  def __init__(self, **kwargs):
-    self.__dict__.update(kwargs)
-  
-  @classmethod
-  def _rest_call(cls, **kwargs):
-    conn = cls._connection
-    
-    try:
-      conn.request(**kwargs)
-      response = conn.getresponse()
-    except: #should call conn.close() on any error to allow further calls to be made
-      conn.close()
-      return None
-    
-    logging.debug('Response code: %s' % response.status)
-    if response.status == 200:
-      continuation_url = cls._continuator(response)
-      data = cls._parser(response.read())
-      if continuation_url:
-        logging.debug('Found more at: ' + continuation_url)
-        kwargs['url'] = continuation_url
-        data += cls._rest_call(**kwargs)
-      
-      return data
-  
-  @classmethod
-  def get(cls, id, **kwargs):
-    kwargs.update(dict(id=quote(id)))
-    path = cls._path % kwargs
-    data = cls._rest_call(method='GET', url=path) or {}
-    instance = cls(**data)
-    instance._id = id
-    instance._get_params = kwargs
-    return instance
 
 
 class WrappedList(list):
   def __init__(self, iterable, wrapper):
     super(self.__class__, self).__init__(iterable)
     self.__wrapper = wrapper
-  
+
   @staticmethod
   def is_dict(obj):
     return isinstance(obj, dict)
-  
+
   def __getitem__(self, key):
     item = super(self.__class__, self).__getitem__(key)
-    should_wrap = self.isdict(item) or isinstance(key, slice) and any(map(self.isdict, item))
+    should_wrap = self.is_dict(item) or isinstance(key, slice) and any(map(self.is_dict, item))
     if should_wrap:
       item = map(self.__wrapper, item) if isinstance(key, slice) \
         else self.__wrapper(item)
       self[key] = item
-    
+
     return item
 
   def __getslice__(self, i, j):
     items = super(self.__class__, self).__getslice__(i, j)
-    if any(map(self.isdict, items)):
+    if any(map(self.is_dict, items)):
       items = map(self.__wrapper, items)
       self[i:j] = items
     return items
@@ -95,8 +53,12 @@ class WrappedList(list):
     return (self.__wrapper(item) for item in iterator)
 
 
-class Many(object):
-  def __init__(self, model, path = None):
+class Relation(object):
+  pass
+
+
+class Many(Relation):
+  def __init__(self, model, path=None):
     self.__model = model
     self.__path = path or model._path
     self.__cache = {}
@@ -111,26 +73,100 @@ class Many(object):
       elif isinstance(data, self.__model):
         return data
     return mapper
-  
-  def _get_id_dict(self, owner):
-    ids = {}
-    while owner:
-      ids[owner.__class__.__name__.lower()] = owner._id
-      owner = getattr(owner, '_owner', None)
-    return ids
-  
+
   def __get__(self, instance, owner):
+    if not instance:
+      return self.__model
+
     if instance not in self.__cache:
       model = self.__model
       if not instance:
         return model
-      
-      path_params = self._get_id_dict(instance)
+
+      path_params = instance._get_id_dict()
       if hasattr(instance, '_get_params'):
         path_params.update(instance._get_params)
       path = self.__path % path_params
-      
+
       logging.debug('Call many path: %s' % path)
       data = model._rest_call(method='GET', url=path) or []
       self.__cache[instance] = WrappedList(data, self._with_owner(instance))
     return self.__cache[instance]
+
+
+class Foreign(Relation):
+  def __init__(self, model, key_extractor=None):
+    self.__model = model
+    self.__key_extractor = key_extractor if key_extractor else \
+      lambda x:dict(id=x.__dict__['__' + model.__name__.lower()])
+
+    self.__cache = {}
+
+  def __get__(self, instance, owner):
+    if not instance:
+      return self.__model
+
+    if instance not in self.__cache:
+      keys = instance._get_id_dict()
+      keys.update(self.__key_extractor(instance))
+      self.__cache[instance] = self.__model.get(**keys)
+
+    return self.__cache[instance]
+
+
+class Model(object):
+  __metaclass__ = ModelBase
+  _secure = True
+  _continuator = lambda x, y:None
+  _parser = staticmethod(json.loads)
+
+  def __init__(self, **kwargs):
+    self.__dict__.update(kwargs)
+    cls = self.__class__
+    overlaps = set(cls.__dict__) & set(kwargs)
+    logging.debug('Found overlaps: ' + str(overlaps))
+    for item in overlaps:
+      if issubclass(getattr(cls, item), Model):
+        self.__dict__['__' + item] = self.__dict__.pop(item)
+
+  def _get_id_dict(self):
+    ids = {}
+    owner = self
+    while owner:
+      ids[owner.__class__.__name__.lower()] = owner._id
+      owner = getattr(owner, '_owner', None)
+    return ids
+
+  @classmethod
+  def _rest_call(cls, **kwargs):
+    conn = cls._connection
+
+    try:
+      conn.request(**kwargs)
+      response = conn.getresponse()
+    except: #should call conn.close() on any error to allow further calls to be made
+      conn.close()
+      return None
+
+    logging.debug('Response code: %s' % response.status)
+    if response.status == 200:
+      continuation_url = cls._continuator(response)
+      data = cls._parser(response.read())
+      if continuation_url:
+        logging.debug('Found more at: ' + continuation_url)
+        kwargs['url'] = continuation_url
+        data += cls._rest_call(**kwargs)
+
+      return data
+
+  @classmethod
+  def get(cls, id, **kwargs):
+    kwargs.update(dict(id=quote(id)))
+    path = cls._path % kwargs
+    data = cls._rest_call(method='GET', url=path) or {}
+
+    instance = cls(**data)
+    instance._id = id
+    instance._get_params = kwargs
+    return instance
+
