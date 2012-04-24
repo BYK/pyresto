602c4a5193d1f8e7264de1c743da2e715bf6a204
Berker Peksag <berker.peksag@gmail.com>
2012-04-24 23:14:40 +0300

    Added setup.py for PyPI release.

--- README.rst
+++ README.rst
@@ -14,7 +14,17 @@ Pyresto
 Installation
 ------------
 
-To install Pyresto, simply: ::
+To install Pyresto, simply::
 
     $ pip install pyresto
+
+
+License
+-------
+
+All files that are part of this project are covered by the following license, except where explicitly noted.::
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import pyresto
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+# Taken from kennethreitz/requests/setup.py
+package_directory = os.path.realpath(os.path.dirname(__file__))
+
+def get_file_contents(file_path):
+    """Get the context of the file using full path name."""
+    full_path = os.path.join(package_directory, file_path)
+    return open(full_path, 'r').read()
+
+setup(
+    name=pyresto.__title__,
+    version=pyresto.__version__,
+    description='A general REST based ORM. ',
+    long_description=get_file_contents('README.rst'),
+    author='Burak Yiğit Kaya',
+    author_email='ben@byk.im',
+    maintainer='Berker Peksag',
+    maintainer_email='berker.peksag@gmail.com',
+    url='http://pyresto.readthedocs.org',
+    packages=[
+        'pyresto',
+        'pyresto.apis'
+    ],
+    license='Mozilla Public License, v. 2.0',
+    classifiers=(
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
