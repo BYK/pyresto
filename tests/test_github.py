61802840164eaeb91d2a008c323b187993a0a62d
Berker Peksag <berker.peksag@gmail.com>
2012-06-26 03:29:26 +0300

    Fixed a build error.

--- tests/test_github.py
+++ tests/test_github.py
@@ -4,15 +4,15 @@
 import unittest
 
 import pyresto
-import pyresto.apis.GitHub as GitHub
+import pyresto.apis.github as GitHub
 
 
 class GitHubTestSuite(unittest.TestCase):
     def test_user_repos(self):
-        """Expects pyresto.apis.GitHub.User."""
+        """Expects pyresto.apis.github.User."""
         user = GitHub.User.get('berkerpeksag')
         # assertIsInstance added in 2.7.
-        self.assertTrue(isinstance(user, pyresto.apis.GitHub.User))
+        self.assertTrue(isinstance(user, pyresto.apis.github.User))
 
 if __name__ == '__main__':
     unittest.main()
